2754718aef0c6bef8b25d135d4f7a2aea1492264
ROHAN SRIDHAR <95674406+Ro-han12@users.noreply.github.com>
2023-12-06 22:42:56 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "Kidney-Disease-Classification-Deep-Learning-Project"
-AUTHOR_USER_NAME = "krishnaik06"
+REPO_NAME = "Fasion-recommender-Deep-Learning-Project"
+AUTHOR_USER_NAME = "Ro-han12"
 SRC_REPO = "cnnClassifier"
-AUTHOR_EMAIL = "entbappy73@gmail.com"
+AUTHOR_EMAIL = "rohannair2939@gmail.com"
 
 
 setuptools.setup(
